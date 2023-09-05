2d7038c3f52d483c6c2787c630ad278b36cca594
George Guilherme <george.gui2000@gmail.com>
2023-09-04 23:12:18 -0300

    feat: seeding class

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -2,14 +2,22 @@
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<SalesWebMvcContext>(options =>
+builder.Services.AddDbContextPool<SalesWebMvcContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMvcContext"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SalesWebMvcContext")))
 );
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddScoped<SeedingService>();
+
 var app = builder.Build();
+
+// Código retirado do github: https://github.com/medinasp/SalesWebMvc/blob/main/Program.cs
+//Abaixo exemplo de como rodar a injeção de dependência, no caso o SeedService adicionado acima: "builder.Services.AddScoped<SeedingService>();",
+//Apesar de ter uma lógica para não repopular o banco, vou deixar aqui comentado pra não ficar rodando sempre que executar o programa.
+app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
