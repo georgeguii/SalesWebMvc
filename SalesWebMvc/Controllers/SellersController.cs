5b9aed61188375f93e5298699ae4f6a1bb3159fe
George Guilherme <george.gui2000@gmail.com>
2023-09-05 15:09:17 -0300

    feat: basic Sellers controller

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers;
+public class SellersController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
