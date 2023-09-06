5d173e2ee1d05b568c2af50f33f38e36c14b0c6a
George Guilherme <george.gui2000@gmail.com>
2023-09-05 21:52:32 -0300

    feat: format data and culture info

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Localization;
+
 using SalesWebMvc.Data;
 using SalesWebMvc.Services;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContextPool<SalesWebMvcContext>(options =>
@@ -17,11 +20,20 @@ builder.Services.AddScoped<DepartmentService>();
 
 var app = builder.Build();
 
+var enUs = new CultureInfo("en-US");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(enUs),
+    SupportedCultures = new List<CultureInfo> { enUs },
+    SupportedUICultures = new List<CultureInfo> { enUs }
+};
+
+app.UseRequestLocalization(localizationOptions);
+
 // Código retirado do github: https://github.com/medinasp/SalesWebMvc/blob/main/Program.cs
 //Abaixo exemplo de como rodar a injeção de dependência, no caso o SeedService adicionado acima: "builder.Services.AddScoped<SeedingService>();",
 //Apesar de ter uma lógica para não repopular o banco, vou deixar aqui comentado pra não ficar rodando sempre que executar o programa.
 app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
